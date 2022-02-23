bae8193fc644cdfcb76910ee5595d219d6c7219a
LordSaitamaa <nicolasstader@gmail.com>
2022-02-23 17:55:38 +0100

    Bot Command init

--- Thirain/Commands/CommandBase.cs
+++ Thirain/Commands/CommandBase.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thirain.CommandHandler;
+using Thirain.Data.DataAccess;
 
 namespace Thirain.Commands
 {
-    internal class CommandBase
+    public abstract class CommandBase : ModuleBase<SocketCommandContext>
     {
+        public readonly DataAccessLayer _dal;
+        public ThirainCommandService _service { get; set; }
+        protected CommandBase(DataAccessLayer dal)
+        {
+            _dal = dal;
+        }
     }
 }
---
+++ Thirain/Commands/ConfigCommand.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thirain.Data.DataAccess;
+
+namespace Thirain.Commands
+{
+    public class ConfigCommand : CommandBase
+    {
+        public ConfigCommand(DataAccessLayer dal) : base(dal) { }
+
+
+        [Command("help")]
+        [Alias("hilfe")]
+        public Task HelpAsync()
+        {
+            return ReplyAsync("HALLOOOOO");
+        }
+    }
+}
